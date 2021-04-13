44f2a5236f35037d7c649201d839dace72c1b0c2
unknown <51089431+CSS-Lletya@users.noreply.github.com>
2021-04-13 10:33:27 -0400

    Files rework
    Not too sure what happened, but old files came back. Fixed now.

--- 727 server/src/com/rs/game/npc/familiar/Talonbeast.java
+++
@@ -1,5 +0,0 @@
-package com.rs.game.npc.familiar;
-
-public class Talonbeast {
-
-}
--- 727 server/src/com/rs/game/player/content/dungeoneering/rooms/RoomEvent.java
+++
@@ -1,9 +0,0 @@
-package com.rs.game.player.content.dungeoneering.rooms;
-
-import com.rs.game.player.content.dungeoneering.DungeonManager;
-import com.rs.game.player.content.dungeoneering.RoomReference;
-
-public interface RoomEvent {
-
-	public void openRoom(DungeonManager dungeon, RoomReference reference);
-}
