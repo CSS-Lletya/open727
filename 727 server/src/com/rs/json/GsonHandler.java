017566ff6e7c4736c9db0a07532581e9fb338fd4
unknown <51089431+CSS-Lletya@users.noreply.github.com>
2021-04-28 22:18:02 -0400

    New Shops system added

--- 727 server/src/com/rs/json/GsonHandler.java
+++ 727 server/src/com/rs/json/GsonHandler.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import com.rs.json.impl.NPCAutoSpawn;
 import com.rs.json.impl.ObjectSpawnLoader;
+import com.rs.json.impl.ShopsLoader;
 
 /**
  * @author Tyluur <itstyluur@gmail.com>
@@ -51,6 +52,7 @@ public class GsonHandler {
 	public static void addJsonLoaders() throws InstantiationException, IllegalAccessException {
 		CLASSES.add(NPCAutoSpawn.class.newInstance());
 		CLASSES.add(ObjectSpawnLoader.class.newInstance());
+		CLASSES.add(ShopsLoader.class.newInstance());
 	}
 
 	/**
---
+++ 727 server/src/com/rs/utils/ChatColors.java
@@ -0,0 +1,57 @@
+package com.rs.utils;
+
+/**
+ *
+ * @author Tyluur <itstyluur@gmail.com>
+ * @since 2012-11-14
+ */
+public interface ChatColors {
+
+	public static final String RED = "FF0000",
+
+	DARK_RED = "330000",
+
+	BLUE = "000FF",
+
+	YELLOW = "FFFF33",
+
+	WHITE = "FFFFFF",
+
+	PURPLE = "660066",
+
+	LIGHT_BLUE = "99CCFF",
+
+	MILD_BLUE = "0066CC",
+
+	ORANGE = "FF6600",
+
+	BLACK = "000000",
+
+	GREEN = "006600",
+
+	MAROON = "800000",
+
+	DARK_GREEN = "003300";
+	
+	public static String red = "<col=ff0000>";
+	public static String salmon = "<col=ff3333>";
+	public static String green = "<col=00ff00>";
+	public static String blue = "<col=0000ff>";
+	public static String white = "<col=ffffff>";
+	public static String black = "<col=000000>";
+	public static String orange = "<col=C9A204>";
+	public static String cyan = "<col=00B2ED>";
+	public static String rcyan = "<col=80ffff>";
+	public static String dcyan = "<col=00b3b3>";
+	public static String gray = "<col=A6A6A6>";
+	public static String lightGray = "<col=C4C4C4>";
+	public static String darkRed = "<col=B52100>";
+	public static String yellow = "<col=FFFF00>";
+	public static String brown = "<col=8C6C0D>";
+	public static String gold = "<col=E6CC07>";
+	
+	public static String shad = "<shad=000000>";
+	public static String eshad = "</col></shad>";
+	public static String purple = "<col=de32ff>";
+
+}
